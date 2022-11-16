5632c48941686a9fc558718e99b939a20fd7a389
Virtua Creative <info@virtuacreative.com.br>
2022-11-16 16:45:28 +0000

    Kubectl Apply Traefik deployment

---
+++ kube-scripts-initial.sh
@@ -0,0 +1,5 @@
+kubectl apply -f 00-role.yml \
+              -f 00-account.yml \
+              -f 01-role-binding.yml \
+              -f 02-traefik.yml \
+              -f 02-traefik-services.yml
