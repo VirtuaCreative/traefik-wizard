45a5c25b6a362b8e919d7119a1adbba77a9c9b3f
Marcia Ramos <virtua.creative@gmail.com>
2022-11-18 15:21:19 +0000

    Fix misalignment

--- traefik-install.sh
+++ traefik-install.sh
@@ -12,7 +12,7 @@ mkdir traefik
 curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/00-role.yml > traefik/traefik-rbac-role.yml
 curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/00-account.yml > traefik/traefik-service-account.yml
 curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/01-role-binding.yml > traefik/traefik-role-binding.yml
-curl -s  https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/02-traefik.yml > traefik/traefik-deployment.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/02-traefik.yml > traefik/traefik-deployment.yml
 curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/02-traefik-services.yml > traefik/traefik-services.yml
 curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/03-whoamI-services.yml > traefik/traefik-app-services.yml
 curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/03-whoami.yml > traefik/traefik-app-deployment.yml
