a8938cd7751f719ecdb959d872c121fa03a66d8b
Marcia Ramos <virtua.creative@gmail.com>
2022-11-20 13:16:48 +0000

    Rename files to make them more semantic-friendly

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Proxy Wizard
 
-This repo contains the resource files to launch Traefik Proxy on a Kubernetes cluster.
+This repo contains the resource files to launch Traefik Proxy on a Kubernetes cluster
+and a script to copy the files and apply them at once.
 
-To make our lives easier, instead of having to copy and paste
-them manually from the [official docs](https://doc.traefik.io/traefik/getting-started/quick-start-with-kubernetes/),
+For now, to make our lives easier, instead of having to copy and paste all the resource
+files manually from the [official docs](https://doc.traefik.io/traefik/getting-started/quick-start-with-kubernetes/)
+to launch Traefik Proxy Dashboard and to use it as a reverse proxy to a web app,
 I created a very simple script to copy the files into a directory of our
 choice and apply them to an exixting cluster.
+
+Usage: TBA.
+
+I might add other resource files and scripts in the future.
 
 ENV: Minikube v1.28.0 / Docker 20.10.20 / Kubernetes v1.25.3 / kubectl v1.25.4 / Darwin 13.0.1
---
+++ traefik-proxy-install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+WAVE='\360\237\221\213'
+TADA='\360\237\216\211'
+
+printf "Hello, $USER! $WAVE \n"
+echo "Welcome to the Proxy Wizard! Let's make some magic!"
+echo "Creating Kubernetes resources for Traefik under ./traefik/..."
+
+mkdir traefik
+
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-rbac-role.yml > traefik/traefik-rbac-role.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-service-account.yml > traefik/traefik-service-account.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-role-binding.yml > traefik/traefik-role-binding.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-dashboard-deployment.yml > traefik/traefik-dashboard-deployment.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-services.yml > traefik/traefik-services.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-app-services.yml > traefik/traefik-app-services.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-app-deployment.yml > traefik/traefik-app-deployment.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-app-ingress.yml > traefik/traefik-app-ingress.yml
+
+echo "Applying the new resources to your cluster..."
+
+kubectl apply -f traefik/traefik-rbac-role.yml \
+              -f traefik/traefik-service-account.yml \
+              -f traefik/traefik-role-binding.yml \
+              -f traefik/traefik-dashboard-deployment.yml \
+              -f traefik/traefik-services.yml \
+              -f traefik/traefik-app-services.yml \
+              -f traefik/traefik-app-deployment.yml \
+              -f traefik/traefik-app-ingress.yml
+
+TF_PR_DS=$(kubectl get services | grep traefik-dashboard-service | cut -d " " -f 1)
+WHOAMI=$(kubectl get services | grep whoami | cut -d " " -f 1)
+
+echo `tput bold`"$ kubectl get services"`tput sgr0` ; kubectl get services
+
+if [[ TF_PR_DS=="traefik-dashboard-service" ]]; then
+    echo `tput bold`"Traefik Proxy Dashboard"`tput sgr0` "is up and running."
+    if [[ WHOAMI=="whoami" ]]; then
+        echo `tput bold`"whoami"`tput sgr0` "is up and running."
+    fi
+    printf `tput bold`"You're good to go! Woot! $TADA \n"`tput sgr0`
+else
+    echo "Something went wrong. Please try the manual method."
+fi 
