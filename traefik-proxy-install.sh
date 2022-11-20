4415901fdc26c96c1ff1d4ed61afa48245d292de
Marcia Ramos <virtua.creative@gmail.com>
2022-11-20 14:44:18 +0000

    Fixes and replace app

--- traefik-proxy-install.sh
+++ traefik-proxy-install.sh
@@ -9,14 +9,14 @@ echo "Creating Kubernetes resources for Traefik under ./traefik/..."
 
 mkdir traefik
 
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-rbac-role.yml > traefik/traefik-rbac-role.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-service-account.yml > traefik/traefik-service-account.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-role-binding.yml > traefik/traefik-role-binding.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-dashboard-deployment.yml > traefik/traefik-dashboard-deployment.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-services.yml > traefik/traefik-services.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-app-services.yml > traefik/traefik-app-services.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-app-deployment.yml > traefik/traefik-app-deployment.yml
-curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/kube/traefik-app-ingress.yml > traefik/traefik-app-ingress.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-rbac-role.yml > traefik/traefik-rbac-role.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-service-account.yml > traefik/traefik-service-account.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-role-binding.yml > traefik/traefik-role-binding.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-dashboard-deployment.yml > traefik/traefik-dashboard-deployment.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-services.yml > traefik/traefik-services.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-app-services.yml > traefik/traefik-app-services.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-app-deployment.yml > traefik/traefik-app-deployment.yml
+curl -s https://raw.githubusercontent.com/VirtuaCreative/traefik-wizard/main/proxy-kube-resources/traefik-app-ingress.yml > traefik/traefik-app-ingress.yml
 
 echo "Applying the new resources to your cluster..."
 
@@ -30,14 +30,14 @@ kubectl apply -f traefik/traefik-rbac-role.yml \
               -f traefik/traefik-app-ingress.yml
 
 TF_PR_DS=$(kubectl get services | grep traefik-dashboard-service | cut -d " " -f 1)
-WHOAMI=$(kubectl get services | grep whoami | cut -d " " -f 1)
+HELLOWORLD=$(kubectl get services | grep hello-world | cut -d " " -f 1)
 
 echo `tput bold`"$ kubectl get services"`tput sgr0` ; kubectl get services
 
 if [[ TF_PR_DS=="traefik-dashboard-service" ]]; then
     echo `tput bold`"Traefik Proxy Dashboard"`tput sgr0` "is up and running."
-    if [[ WHOAMI=="whoami" ]]; then
-        echo `tput bold`"whoami"`tput sgr0` "is up and running."
+    if [[ HELLOWORLD=="hello-world" ]]; then
+        echo `tput bold`"hello-world"`tput sgr0` "is up and running."
     fi
     printf `tput bold`"You're good to go! Woot! $TADA \n"`tput sgr0`
 else
